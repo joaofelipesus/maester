517b1486478f3a90976dcf59f3ba32d2c09aaf4d
joaofelipe <joaofelipe1294@gmail.com>
2026-08-18 08:41:42 -0300

    tests: add coverage to validateRequiredConfigs

---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestValidateRequiredConfigs(t *testing.T) {
+	testCases := []struct {
+		name          string
+		cfg           config
+		expectedError bool
+		expected      error
+	}{
+		{
+			name:          "NoMissingParams",
+			cfg:           config{serverUserName: "admin", serverIP: "192.168.1.11"},
+			expectedError: false,
+			expected:      nil,
+		},
+		{
+			name:          "MissingServerUserName",
+			cfg:           config{serverUserName: "", serverIP: "192.168.1.11"},
+			expectedError: true,
+			expected:      errors.New("user tag is required"),
+		},
+		{
+			name:          "MissingIP",
+			cfg:           config{serverUserName: "admin", serverIP: ""},
+			expectedError: true,
+			expected:      errors.New("IP address ir required"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			result := validateRequiredConfigs(testCase.cfg)
+
+			if testCase.expectedError && result.Error() != testCase.expected.Error() {
+				t.Errorf("Expected %q, got %q instead\n", testCase.expected, result)
+			}
+		})
+	}
+}
